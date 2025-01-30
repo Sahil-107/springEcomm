236ea1fb7be2716dedd96c066f97dc53349efaf6
Sahil-107 <messisahil7@gmail.com>
2025-01-30 23:05:32 +0530

    DAY-2 Created new methods

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,15 +1,21 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -18,6 +24,27 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId){
+        Category category = categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst().orElseThrow(()-> new ResponseStatusException(NOT_FOUND, "Resource Not Found"));
+        categories.remove(category);
+        return "Category with categoryId: "+categoryId+ " deleted successfully!";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId){
+        Optional<Category> optionalCategory = categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst();
+
+        if(optionalCategory.isPresent()){
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(NOT_FOUND, "Category not found!");
+        }
     }
 }
