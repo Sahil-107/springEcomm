93b09bef3419f42f60c816f723e8ef65c9e27944
Sahil-107 <messisahil7@gmail.com>
2025-06-18 01:14:39 +0530

    DAY-6 Created pagenation and sorting

--- src/main/java/com/ecommerce/project/PracApplication.java
+++ src/main/java/com/ecommerce/project/PracApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracApplication {
+public class  PracApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracApplication.class, args);
--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.config.AppConstants;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
@@ -19,9 +20,17 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping("/echo")
+    public ResponseEntity<String> echoMessage(@RequestParam(name="message") String message){
+        return new ResponseEntity<>("Echoed message: "+ message, HttpStatus.OK);
+    }
+
     @GetMapping("/public/categories")
-    public ResponseEntity<CategoryResponse> getAllCategories(){
-        CategoryResponse categoryresponse =  categoryService.getAllCategories();
+    public ResponseEntity<CategoryResponse> getAllCategories(@RequestParam(name="pageNumber", defaultValue= AppConstants.PAGE_NUMBER, required=false) Integer pageNumber,
+                                                             @RequestParam(name="pageSize", defaultValue= AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+                                                             @RequestParam(name="sortBy", defaultValue= AppConstants.SORT_CATEGORIES_BY, required = false) String sortBy,
+                                                             @RequestParam(name="sortOrder",  defaultValue= AppConstants.SORT_DIR, required = false) String sortOrder){
+        CategoryResponse categoryresponse =  categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
         return new ResponseEntity<>(categoryresponse, HttpStatus.OK);
     }
 
